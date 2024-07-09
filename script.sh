20785b1ff2ba5480c9a656379dea75477a64767e
Alejandro Benedi <abenedi@hiberus.com>
2024-07-09 09:07:07 +0200

    prueba

--- script.sh
+++ script.sh
@@ -15,7 +15,8 @@ file=$5
 
 function upload_raw {
     echo "Subiendo archivo $file a $server_url"
-    curl -v --user "$username:$password" --upload-file $file "https://nexus.prisamedia.com/repository/raw-test/$file"
+    cat $file
+    #curl -v --user "$username:$password" --upload-file $file "https://nexus.prisamedia.com/repository/raw-test/$file"
 }
 
 upload_raw
